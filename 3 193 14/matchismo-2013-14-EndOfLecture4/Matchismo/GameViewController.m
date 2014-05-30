6098f09c29bbce8ea504ecf7a2b58209b3053cc3
Roger Z <the.roger.zou@gmail.com>
2014-05-30 02:41:06 -0700

    IT WORKS IT WORKS EVERYTHING WORKS SO HAPPYgit add .! HISTORY WORKS TOO YAYYYYgit add .git add .! FUCKING SEGUE ID NOT STORYBOARD ID BUT THAT'S OKAY
    Finally finished with Assignment 3 required tasks #1-9. *fistpumps* so much win right here. I'm so happy. Even though it's 2:45am lol. PROGRESS

--- 3 193 14/matchismo-2013-14-EndOfLecture4/Matchismo/GameViewController.m
+++ 3 193 14/matchismo-2013-14-EndOfLecture4/Matchismo/GameViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "GameViewController.h"
+#import "HistoryViewController.h"
 
 @interface GameViewController ()
 
@@ -189,7 +190,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -197,7 +198,12 @@
 {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    if ([segue.identifier isEqualToString:@"historyID"])
+    {
+     //   HistoryViewController *vc = [segue destinationViewController];
+      //  vc.history = self.pastMoves;
+        [segue.destinationViewController setHistory:self.pastMoves];
+    }
 }
-*/
 
 @end
---
+++ 3 193 14/matchismo-2013-14-EndOfLecture4/Matchismo/HistoryViewController.h
@@ -0,0 +1,15 @@
+//
+//  HistoryViewController.h
+//  Matchismo
+//
+//  Created by Roger on 5/30/14.
+//  Copyright (c) 2014 m2m server software gmbh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoryViewController : UIViewController
+
+@property (nonatomic, strong) NSMutableArray *history;
+
+@end
